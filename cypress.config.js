f550fc24756de7746e6fcc7a12473a0f574e32fb
Reaper Gelera <ahoy@barelyhuman.dev>
2024-03-18 21:24:35 +0530

    fix: remove e2e config

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    experimentalStudio: true,
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
