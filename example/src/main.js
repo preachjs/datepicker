1834041560e3bf1e8f2b7a6ce72400b20bca228b
Reaper Gelera <ahoy@barelyhuman.dev>
2024-03-25 15:01:51 +0530

    remove locale from exampel

--- example/src/main.js
+++ example/src/main.js
@@ -37,7 +37,6 @@ const App = () => {
           </label>
         </div>
         <Calendar
-          locale="ban-id"
           mode={mode.value}
           value={selectedDate.value}
           onSelect={(value) => {
