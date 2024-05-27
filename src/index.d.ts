d44b8ddbaa04019332d36a10dfcb4b4d6a22fb7e
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2024-05-28 01:05:02 +0300

    feat: add typescript support

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { JSX } from "preact"
+import { CalendarMode, CalendarProps } from "./calendar"
+
+export const Calendar: <M extends CalendarMode>(props: CalendarProps<M>) => JSX.Element
+
+export * from "./calendar"
