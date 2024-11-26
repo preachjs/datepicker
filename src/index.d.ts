9b1aa06e19b55746a661ae8bdef67bc26e77aeee
reaper <ahoy@barelyhuman.dev>
2024-11-26 17:15:16 +0530

    chore: rexport prop types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 import { JSX } from 'preact'
-import { CalendarMode, CalendarProps } from './calendar'
+import type { CalendarMode, CalendarProps } from './calendar'
+export type { CalendarMode, CalendarProps } from './calendar'
 
 export const Calendar: <M extends CalendarMode>(
   props: CalendarProps<M>
